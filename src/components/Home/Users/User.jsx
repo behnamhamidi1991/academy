1001d37b98dcb7b6fc5134e3ba71eb047a4f96cf
Behnam Hamidi <eslp91@hotmail.com>
2024-04-06 01:01:49 +0330

    check and finish

--- src/components/Home/Users/User.jsx
+++ src/components/Home/Users/User.jsx
@@ -15,6 +15,32 @@ const User = () => {
         spaceBetween={10}
         modules={[Pagination]}
         className="userContainer"
+        breakpoints={{
+          0: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          371: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+          },
+          540: {
+            slidesPerView: 7,
+            spaceBetween: 10,
+          },
+          780: {
+            slidesPerView: 9,
+            spaceBetween: 10,
+          },
+          1133: {
+            slidesPerView: 12,
+            spaceBetween: 10,
+          },
+          1454: {
+            slidesPerView: 17,
+            spaceBetween: 10,
+          },
+        }}
       >
         {users.map((item) => (
           <SwiperSlide key={item.id} className="singleUser">
