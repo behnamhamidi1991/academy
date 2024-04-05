401d9cd086eec615cc759f19f6d2ddb6a6fe5017
Behnam Hamidi <eslp91@hotmail.com>
2024-04-05 14:55:11 +0330

    change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,35 +4,50 @@ import { toggle } from "../../features/themeSlice";
 import logo from "../../assets/icons/acicon.png";
 import { FaUser } from "react-icons/fa6";
 import { IoMoonSharp } from "react-icons/io5";
-import { MdShoppingCart } from "react-icons/md";
+import { FaShoppingCart } from "react-icons/fa";
+import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const theme = useSelector((state) => state.theme.dark);
   const dispatch = useDispatch();
 
   return (
-    <header>
-      <div className="headerWrapper">
-        <div className="header-logo-links">
-          <div className="header-logo">
-            <img src={logo} alt="logo" />
-            <p>Deep Learn Academy</p>
-          </div>
-          <ul className="header-links">
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Contact</li>
-            <li>About</li>
-          </ul>
-        </div>
-        <div className="header-countdown">
-          <span>00</span>: <span>00</span>: <span>00</span>: <span>00</span>
-        </div>
-        <div className="header-icons">
-          <FaUser />
-          <IoMoonSharp />
-          <MdShoppingCart />
-        </div>
+    <header className={theme ? "headerWrapper dark" : "headerWrapper light"}>
+      <div className="header-logo-links">
+        <Link to="/" className="header-logo">
+          <img src={logo} alt="logo" className="header-logo-image" />
+          <p>Deep Learn Academy</p>
+        </Link>
+        <ul className="header-links">
+          <Link to="/" className="nav-item">
+            Home
+          </Link>
+          <button className="nav-item navBtn">Menu</button>
+          <Link to="/" className="nav-item">
+            Blog
+          </Link>
+          <Link to="/" className="nav-item">
+            About
+          </Link>
+          <Link to="/" className="nav-item">
+            Contact
+          </Link>
+        </ul>
+      </div>
+      <div className="header-countdown">
+        <span>00</span>: <span>00</span>: <span>00</span>: <span>00</span>
+      </div>
+      <div className="header-icons">
+        <button>
+          <FaUser className="header-icons" />
+        </button>
+        <button onClick={() => dispatch(toggle(!theme))}>
+          <IoMoonSharp className="header-icons" />
+        </button>
+        <button>
+          <FaShoppingCart className="header-icons" />
+        </button>
       </div>
     </header>
   );
