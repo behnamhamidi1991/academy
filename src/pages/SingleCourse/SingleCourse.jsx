3518d1bfdf9a1e2186385e327e5f58f631ca2a2e
Behnam Hamidi <eslp91@hotmail.com>
2024-04-16 11:50:46 +0330

    routing

---
+++ src/pages/SingleCourse/SingleCourse.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SingleCourse = () => {
+  return (
+    <div>
+      <h1>SINGLE COURSE PAGE</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        reiciendis ducimus provident nemo iusto, dolores reprehenderit
+        accusamus, nisi quas tempora mollitia optio velit nostrum illum. Eveniet
+        consectetur corporis at molestias?
+      </p>
+    </div>
+  );
+};
+
+export default SingleCourse;
