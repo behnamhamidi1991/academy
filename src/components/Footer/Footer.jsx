36d091a589646fcfd6b98a4939daa63ba24deec6
Behnam Hamidi <eslp91@hotmail.com>
2024-04-16 23:07:34 +0330

    go check

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      FOOTER
+      <div className="footer-left"></div>
+      <div className="footer-navlinks"></div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import { useSelector } from "react-redux";
 import Offer from "../components/Header/Offer/Offer";
+import Footer from "../components/Footer/Footer";
 
 const AppLayout = () => {
   const theme = useSelector((state) => state.theme.dark);
@@ -19,6 +20,8 @@ const AppLayout = () => {
       <main className={theme ? "main dark" : "main light"}>
         <Outlet />
       </main>
+
+      <Footer />
     </div>
   );
 };
